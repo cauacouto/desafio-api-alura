399c62699d2a239ccc8000a529c47d0600321998
cauacouto <cauacouto022@gmail.com>
2026-06-07 18:43:11 -0300

    add ReservaMapper interface for mapping between Reserva and DTOs

---
+++ src/main/java/couto/dev/desafio/alura/Mapper/ReservaMapper.java
@@ -0,0 +1,29 @@
+package couto.dev.desafio.alura.Mapper;
+
+import couto.dev.desafio.alura.Dto.ReservaRequestDto;
+import couto.dev.desafio.alura.Dto.ReservaResponseDto;
+import couto.dev.desafio.alura.Dto.SalaRequestDto;
+import couto.dev.desafio.alura.Dto.SalaResponseDto;
+import couto.dev.desafio.alura.domin.Reserva;
+import couto.dev.desafio.alura.domin.Sala;
+import org.mapstruct.MapMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface ReservaMapper {
+
+
+  @Mapping(target = "dataInicio",source = "dataInicio")
+  @Mapping(target = "dataFim",source = "dataFim")
+  @Mapping(target = "usuario",source = "usuario.id")
+  @Mapping(target = "sala",source = "sala.id")
+  @Mapping(target = "statusReserva",source = "status")
+
+  ReservaResponseDto toDto(Reserva entity);
+
+
+  Reserva toEntity(ReservaRequestDto dto);
+
+
+}
