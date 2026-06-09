493bb85dbd0f9f082a9a3dafdb8ac2c9f7b93338
cauacouto <cauacouto022@gmail.com>
2026-06-08 22:49:35 -0300

    add JsonFormat annotations to ReservaRequestDto and ReservaResponseDto for date fields; update field names for consistency

--- src/main/java/couto/dev/desafio/alura/Dto/ReservaRequestDto.java
+++ src/main/java/couto/dev/desafio/alura/Dto/ReservaRequestDto.java
@@ -1,10 +1,14 @@
 package couto.dev.desafio.alura.Dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record ReservaRequestDto(
+        @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
         LocalDateTime dataInicio,
+        @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
         LocalDateTime dataFim,
         Integer SalaId,
         UUID UsuarioId
--- src/main/java/couto/dev/desafio/alura/Dto/ReservaResponseDto.java
+++ src/main/java/couto/dev/desafio/alura/Dto/ReservaResponseDto.java
@@ -1,15 +1,18 @@
 package couto.dev.desafio.alura.Dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import couto.dev.desafio.alura.Enum.statusReserva;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record ReservaResponseDto(
+        @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
         LocalDateTime dataInicio,
+        @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
         LocalDateTime dataFim,
-        Integer SalaId,
-        UUID UsuarioId,
+        Integer salaId,
+        UUID usuarioId,
         statusReserva status
 
 ) {
