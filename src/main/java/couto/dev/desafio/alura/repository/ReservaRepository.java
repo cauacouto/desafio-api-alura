760500e69a597b11df99b47e907412f9be81c901
cauacouto <cauacouto022@gmail.com>
2026-06-08 22:49:16 -0300

    add ReservaRepository: implement method to check for reservation conflicts

--- src/main/java/couto/dev/desafio/alura/repository/ReservaRepository.java
+++ src/main/java/couto/dev/desafio/alura/repository/ReservaRepository.java
@@ -1,7 +1,20 @@
 package couto.dev.desafio.alura.repository;
 
 import couto.dev.desafio.alura.domin.Reserva;
+import couto.dev.desafio.alura.domin.Sala;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
 
 public interface ReservaRepository extends JpaRepository<Reserva,Integer> {
+
+    @Query("select count(r) > 0 from Reserva r " +
+            "where r.sala = :sala and r.status = 'RESERVADA' " +
+            "and r.dataInicio < :dataFimComIntervalo and r.dataFim > :dataInicioMenosIntervalo")
+    boolean temConflito(@Param("sala") Sala sala,
+                        @Param("dataInicioMenosIntervalo") LocalDateTime dataInicioMenosIntervalo,
+                        @Param("dataFimComIntervalo") LocalDateTime dataFimComIntervalo);
+
 }
